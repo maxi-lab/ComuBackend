f9ca88fa620b7171b8ba16943f7a419ab0499e85
Valentino Chiappini <valentino.chiappini10@gmail.com>
2025-06-19 10:28:26 -0300

    URL HOME API modificada por error 404

--- backend/urls.py
+++ backend/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('audio_api.urls')),  # Include the audio API URLs
+    path('', include('audio_api.urls')),  # Include the audio API URLs
 ]
