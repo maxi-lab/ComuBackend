93949d9047dccc1621aab183d64b9207f9c4be59
Valentino Chiappini <valentino.chiappini10@gmail.com>
2025-06-20 18:14:09 -0300

    Retorno el ultimo audio al front (v2)

--- backend/settings.py
+++ backend/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  # Add this for Django REST Framework
     'audio_api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Debe estar primero
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",  # tu frontend
+    "http://localhost:5500", 
+    "http://localhost:5173" #npm run dev
+]
