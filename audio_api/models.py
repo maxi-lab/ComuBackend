2d259c533be5fb50ee4bdf39317dc093061d1c7a
Maximo Preneste <maxipreneste@gmail.com>
2025-07-05 21:46:05 -0300

    feat(models):agregar columna de cuantizacion

--- audio_api/models.py
+++ audio_api/models.py
@@ -7,6 +7,7 @@ class AudioFile(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True)
     audio_file = models.FileField(upload_to='audio/') # Files will be stored in media/audio/
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    cuantizacion= models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.title if self.title else f"Audio File {self.id}"
